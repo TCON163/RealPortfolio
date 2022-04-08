80146e787d9d1e129890d220fd40c2487c9d95a9
unknown <tylerconnercareersearch@gmail.com>
2022-04-07 20:02:03 -0400

    set up the landing page

--- real-portfolio/src/App.js
+++ real-portfolio/src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from "./page/HomePage";
 import './App.css';
-
+import PortCube from "./page/Cube/PortCube";
 
 function App() {
   return (
     <Router>
     <Routes>
         {/* the home path route needs the exact keyword or all request will get HomePage */}
-        <Route exact path='/' element={<HomePage/>} />
-  
+        <Route  path='/home' element={<HomePage/>} />
+        <Route exact path="/" element={<PortCube/>} />
 
         
     </Routes>
--- real-portfolio/src/page/Cube/PortCube.js
+++ real-portfolio/src/page/Cube/PortCube.js
@@ -1,51 +1,55 @@
 import * as React from 'react';
 import './PortCube.css'
 import Cube from 'react-3d-cube';
-import one from '../assets/1.PNG';
-import two from '../assets/2.PNG';
-import three from '../assets/3.PNG';
-import four from '../assets/4.PNG';
-import five from '../assets/5.PNG';
-import six from '../assets/6.PNG';
+import one from '../../assets/DJAmplifire.PNG';
+import two from '../../assets/outlook.PNG';
+import three from '../../assets/Nova.PNG';
+import four from '../../assets/Airline.PNG';
+import five from '../../assets/SurvivorPool.PNG';
+import six from '../../assets/Github.PNG';
+import { Link } from 'react-router-dom';
 
 
 class PortCube extends React.Component {
   render() {
     return (
-      <div>
+      <div className='wall'>
+        <h1 className='banner'>Tyler Conner - FullStack Software Engineer</h1>
+        <h3 className='sub-banner'>(Rotate cube to check out some projects.)</h3>
         <center>
-            <h1>react-3d-cube</h1>
-            <br></br>
-            <br></br>
+            
             <div
             style={{
-                width: 400,
-                height: 400
+                width: 300,
+                height: 300
             }}
             >
 
-            <Cube size={400} index="front">
+            <Cube size={300} index="front">
                 <div>
-                  <img src={one} alt="one" style={{width: '400px', height: '400px'}} />
+                  <img src={one} alt="one" style={{width: '300px', height: '300px'}} />
                 </div>
                 <div>
-                  <img src={two} alt="two" style={{width: '400px', height: '400px'}}/>
+                  <img src={two} alt="two" style={{width: '300px', height: '300px'}}/>
                 </div>
                 <div>
-                <img src={three} alt="two" style={{width: '400px', height: '400px'}}/>
+                <img src={three} alt="two" style={{width: '300px', height: '300px'}}/>
                 </div>
                 <div>
-                <img src={four} alt="two" style={{width: '400px', height: '400px'}}/>
+                <img src={four} alt="two" style={{width: '300px', height: '300px'}}/>
                 </div>
                 <div>
-                <img src={five} alt="two" style={{width: '400px', height: '400px'}}/>
+                <img src={five} alt="two" style={{width: '300px', height: '300px'}}/>
                 </div>
                 <div>
-                <img src={six} alt="two" style={{width: '400px', height: '400px'}}/>
+                <img src={six} alt="two" style={{width: '300px', height: '300px'}}/>
                 </div>
             </Cube>
             </div>
+            <button className='proceed'><Link to="/home"> Proceed to HomePage</Link></button>
         </center>
+
+        
       </div>
     );
   }
