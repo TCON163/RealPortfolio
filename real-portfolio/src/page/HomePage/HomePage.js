ddcfabe641df924a9104b9555d74efcd422493d6
unknown <tylerconnercareersearch@gmail.com>
2022-04-09 01:48:31 -0400

    home page's grid is setup

--- real-portfolio/src/page/HomePage/HomePage.js
+++ real-portfolio/src/page/HomePage/HomePage.js
@@ -5,7 +5,24 @@ function HomePage(){
     return (
         <div className="wall">
             <NavBar />
-            <h1>Hello World</h1>
+            <div className="grid-home">
+                <div className="about-me">
+                    <span className="at">@</span><span className="bout-me">bout me</span>
+                </div>
+                <div className="general-info">
+                    <p>general</p>
+                </div>
+                <div className="pics">
+                    <p>pics</p>
+                </div>
+                <div className="work-experince">
+                    <p>work experince</p>
+                </div>
+                <div className="display-links">
+                    <p>links</p>
+                </div>
+                
+            </div>
         </div>
     );
 }
